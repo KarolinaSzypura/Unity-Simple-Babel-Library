bc4f95bd34c52db9deac383768f771e5b162fdca
SzypKa <45697231+KarolinaSzypura@users.noreply.github.com>
2020-07-25 14:47:34 +0200

    Add files

---
+++ Assets/teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public Transform do_teleport;
+    public GameObject gracz;
+
+    void OnTriggerEnter(Collider other)
+    {
+        gracz.transform.position = do_teleport.transform.position;
+        gracz.transform.rotation = do_teleport.transform.rotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
